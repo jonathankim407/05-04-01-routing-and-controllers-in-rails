f1644b8776f7f722bd5f0db58dc3bf5693b733a6
jonathankim407 <jonathankim407@gmail.com>
2017-06-08 16:02:03 -0400

    Route Helpers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,7 +12,7 @@ class PagesController < ApplicationController
 
   def contest
     flash[:notice] = "Sorry, the contest has ended"
-    redirect_to "/welcome"
+    redirect_to welcome_path
   end
 
   # def kitten
@@ -41,7 +41,7 @@ class PagesController < ApplicationController
   def secrets
     if params[:magic_word] != "hello"
       flash[:alert] = "Sorry, you're not authorized to see that page!"
-      redirect_to "/"
+      redirect_to welcome_path
     end
   end
 
