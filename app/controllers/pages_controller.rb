4834dc6159f1d1e299c5bd7b735df0ebf7669dad
jonathankim407 <jonathankim407@gmail.com>
2017-06-08 14:10:30 -0400

    Making the About/Contest Page Work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,15 @@
 class PagesController < ApplicationController
 
   def welcome
+    render :welcome
+  end
+
+  def about
     render :about
+  end
+
+  def contest
+    render :contest
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
   get '/welcome' => 'pages#welcome'
-  
+
   get '/' => 'pages#welcome'
+
+  get '/about' => 'pages#about'
+
+  get '/contest' => 'pages#contest'
 
 end
